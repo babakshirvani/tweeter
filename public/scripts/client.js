354e21a59723e197069d27f602caaf1bffd48040
Babak Shirvani <bk.shirvani@gmail.com>
2021-09-08 23:46:59 +0000

    used ajax to GET data as JSON  on client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,52 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
 
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  for (const tweet of tweets) {
-    // calls createTweetElement for each tweet
-    const $tweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $(`#tweet-container`).prepend($tweet);
-  }
-};
+  const renderTweets = (tweets) => {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $(`#tweet-container`).append($tweet);
+    }
+  };
 
 
-const createTweetElement = function(tweet) {
+  const createTweetElement = (tweet) => {
 
-
-  // const $tweet = $(`<article>`).addClass(`tweet`)
-
-  let $tweet = $(`<article class= "tweet">
+    let $tweet = $(`<article class= "tweet">
     <header>
       <div class="user-detail" >
         <img class="avatar" src="${tweet.user.avatars}">
@@ -73,12 +70,23 @@ const createTweetElement = function(tweet) {
     </div>
     </footer>
   </article>`);
-  return $tweet
-}
+    return $tweet;
+  }
 
 
 
-$(document).ready(function() {
-  renderTweets(data)
-
+  const loadTweets = (tweet) => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      succss: (tweet) => {
+        renderTweets(tweet);
+      },
+      error: (err) => {
+        console.log(`Error: ${err}`);
+      }
+    });
+  };
+  loadTweets();
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,5 +7,20 @@ $(document).ready(function() {
     140 - textLength < 0 ? count.css('color', 'red') : count.css('color', '');
   });
 
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    console.log("The form has been submitted!!");
+
+    const serializedData = $(this).serialize();
+    console.log("serializedData:", serializedData);
+
+    $.post('/tweets', serializedData, (response) => {
+      console.log("response:", response);
+
+    })
+
+  });
+
 });
 
