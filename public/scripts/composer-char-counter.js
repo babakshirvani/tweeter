b5440112a3146ad7b2efae1127cc55c8c231cbd7
Babak Shirvani <bk.shirvani@gmail.com>
2021-09-08 00:22:29 +0000

    Add word counters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $('.new-tweet textarea').on('input', function() {
+    const textLength = $(this).val().length;
+    const count = $(this).parent().children('div').children(".counter");
+    count.text(140 - textLength);
+    140 - textLength < 0 ? count.css('color', 'red') : count.css('color', '');
+  });
+
+});
+
