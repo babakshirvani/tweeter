31c5730076eb0d4d80671593bddb719ac566e0bf
Babak Shirvani <bk.shirvani@gmail.com>
2021-09-09 03:02:27 +0000

    Add feature Preventing XSS with Escaping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,87 +6,87 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+  const validateForm = (text) => {
+    if (!text.length) {
+      alert("Dont try to send an empty tweet");
+      return false;
     }
-  ];
+    if (text.length > 140) {
+      alert("Sorry the limit is 140 characters.");
+      return false
+    }
 
+    return true;
+
+  }
 
   const renderTweets = (tweets) => {
+    const $TweetContainer = $('#tweet-container');
+    $TweetContainer.empty();
+
     // loops through tweets
-    for (const tweet of tweets) {
+    for (let tweet of tweets) {
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet);
       // takes return value and appends it to the tweets container
-      $(`#tweet-container`).append($tweet);
+      $(`#tweet-container`).prepend($tweet);
     }
   };
 
-
   const createTweetElement = (tweet) => {
-
-    let $tweet = $(`<article class= "tweet">
-    <header>
-      <div class="user-detail" >
-        <img class="avatar" src="${tweet.user.avatars}">
-        <div class="user-info">
-          <span>${tweet.user.name}</span>
-        </div>
+    const { avatars, name, handle } = tweet.user;
+    const { text } = tweet.content;
+    const timeStamp = tweet.created_at;
+    const $header = `<header>
+    <div class="user-detail" >
+      <img class="avatar" src="${avatars}">
+      <div class="user-info">
+        <span>${name}</span>
       </div>
-      <small class="user-handle">${tweet.user.handle}</small>
-    </header>
-    <div class="tweet-body">
-      <span >${tweet.content.text}</span>
-    </div>  
-    <footer>
-      <div class="tweet-footer">
-        <small class="date"> ${timeago.format(tweet.created_at)}</small>
-        <span class="reaction">
-          <i class="fas fa-heart"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-flag"></i>
-        </span>
     </div>
-    </footer>
-  </article>`);
-    return $tweet;
+    <small class="user-handle">${handle}</small>
+  </header>`;
+
+    const $footer = `<footer>
+  <div class="tweet-footer">
+    <small class="date"> ${timeago.format(timeStamp)}</small>
+    <span class="reaction">
+      <i class="fas fa-heart"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-flag"></i>
+    </span>
+</div>
+</footer>`;
+    const $tweetBody = $('<div>').addClass('tweet-body')
+    const $tweetText = $('<span>').text(text);
+    const $artile = $('<article>').addClass('tweet');
+
+    return $artile.append($header, $tweetBody.append($tweetText), $footer)
   }
 
-
-
-  const loadTweets = (tweet) => {
-    $.ajax({
-      url: '/tweets',
-      method: 'GET',
-      dataType: 'json',
-      succss: (tweet) => {
-        renderTweets(tweet);
-      },
-      error: (err) => {
-        console.log(`Error: ${err}`);
-      }
-    });
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET', })
+      .then(renderTweets);
   };
+
   loadTweets();
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    console.log("The form has been submitted!!");
+
+    const enteredText = $(this).children('textarea').val()
+    console.log("enteredText: (length):", enteredText.length)
+
+    if (validateForm(enteredText)) {
+      console.log("The form has been submitted!! - successfully ");
+
+      const serializedData = $(this).serialize();
+      $.post('/tweets', serializedData)
+        .then(loadTweets)
+        .then($(this).children('textarea').val(""))
+
+    }
+  });
+
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,44 +1,11 @@
 
 $(document).ready(function() {
 
-  const validateForm = (text) => {
-    if (!text.length) {
-      alert("Dont try to send an empty tweet");
-      return false;
-    }
-    if (text.length > 140) {
-      alert("Sorry the limit is 140 characters.");
-      return false
-    }
-
-    return true;
-
-  }
-
-
   $('.new-tweet textarea').on('input', function() {
     const textLength = $(this).val().length;
     const count = $(this).parent().children('div').children(".counter");
     count.text(140 - textLength);
     140 - textLength < 0 ? count.css('color', 'red') : count.css('color', '');
-  });
-
-
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    console.log("The form has been submitted!!");
-
-    const enteredText = $(this).children('textarea').val()
-
-    console.log("enteredText: (length):", enteredText.length)
-    if (validateForm(enteredText)) {
-      console.log("The form has been submitted!! - successfully ");
-      const serializedData = $(this).serialize();
-      $.post('/tweets', serializedData, (response) => {
-        console.log("response:", response);
-
-      })
-    }
   });
 
 });
